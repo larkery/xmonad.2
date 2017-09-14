f8a12a3c39cca115c909bb0647a08f69f7ff8372
Tom Hinton <t@larkery.com>
2017-09-14 22:07:43 +0100

    more old keys back

--- xmonad.hs
+++ xmonad.hs
@@ -7,6 +7,8 @@ import XMonad.Layout.TwoPane
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.Menu.Menus
 import XMonad.Hooks.DynamicLog
+import XMonad.Actions.CycleWS
+import qualified XMonad.StackSet as W
 
 main = xmonad mconfig
 
@@ -43,11 +45,20 @@ mkeys =
     ( "M-w", spawn "chromium" )
   , ( "M-e", spawn "emacsclient -c -n" )
   , ( "M-;", windowMenu )
-  , ( "M-p", commandMenu )
-  , ( "M-S-;", workspaceMenu )
+  , ( "M-/", commandMenu )
+  , ( "M-'", workspaceMenu )
   , ( "<XF86AudioRaiseVolume>", spawn "pamixer -i 10" )
   , ( "<XF86AudioLowerVolume>", spawn "pamixer -d 10" )
   , ( "<XF86AudioMute>", spawn "pamixer -t" )
   , ( "<XF86MonBrightnessUp>", spawn "xbacklight -inc 10" )
   , ( "<XF86MonBrightnessDown>", spawn "xbacklight -dec 10" )
+
+  , ( "M-n", windows $ W.focusDown )
+  , ( "M-p", windows $ W.focusUp )
+  , ( "M-S-n", windows $ W.swapDown )
+  , ( "M-S-p", windows $ W.swapUp )
+  , ( "M-k", kill )
+
+  , ("M-s", swapNextScreen)
+  , ("M-S-s", shiftNextScreen)
   ]
